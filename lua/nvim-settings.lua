ee03f187faac8d7c2425e2deafbb2a93ae1aa121
MostafaEzzatt <psmostafa@gmail.com>
2024-02-02 08:20:46 -0500

    add auto-completions

--- lua/nvim-settings.lua
+++ lua/nvim-settings.lua
@@ -3,3 +3,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+
+-- article about number and relative number setting
+-- https://jeffkreeftmeijer.com/vim-number/
+vim.cmd("set number")
+vim.cmd("set relativenumber")
